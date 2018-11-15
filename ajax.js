2dd948fb5b3ca3d0bd80234292a7a9c5ed35556f
Dean Harris <dean.harris@comparethemarket.com.au>
2018-11-16 09:39:08 +1000

    Initial commit

---
+++ ajax.js
@@ -0,0 +1,79 @@
+class ajax {
+
+    /**
+     * A simple API to make a request for data from an endpoint. Returns a
+     * promise that resolves with the response text
+     *
+     * @param {string} endpoint The URL to which the request will be made
+     * @param {object} headers Any custom headers that are required to be sent
+     * with the request
+     *
+     * @returns Promise
+     */
+    static get( endpoint, headers = {} ) {
+        return new Promise( ( resolve, reject ) => {
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function() {
+                if ( 4 === xhr.readyState ) {
+                    if ( 200 <= xhr.status && 300 > xhr.status ) {
+                        resolve( xhr.response );
+                    }
+                    else {
+                        reject( xhr.status );
+                    }
+                }
+            };
+
+            if ( headers ) {
+                Object.keys( headers ).forEach( function( header ) {
+                    xhr.setRequestHeader( header, headers[ header ] );
+                } );
+            }
+
+            xhr.open( endpoint, 'GET' );
+
+            xhr.send( null );
+        } );
+    }
+
+    /**
+     * A simple API to send data to an endpoint. Returns a promise that resolves
+     * with the response text
+     *
+     * @param {string} endpoint The URL to which the request will be made
+     * @param {any} payload The data to be sent as a part of the request. Sent
+     * raw, so manipulate it any way it needs to be manipulated before you send
+     * @param {object} headers Any custom headers that are required to be sent
+     * with the request
+     */
+    static post( endpoint, payload, headers = {} ) {
+        return new Promise( ( resolve, reject ) => {
+            let xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function() {
+                if ( 4 === xhr.readyState ) {
+                    if ( 200 <= xhr.status && 300 > xhr.status ) {
+                        resolve( xhr.response );
+                    }
+                    else {
+                        reject( xhr.status );
+                    }
+                }
+            };
+
+            if ( headers ) {
+                Object.keys( headers ).forEach( function( header ) {
+                    xhr.setRequestHeader( header, headers[ header ] );
+                } );
+            }
+
+            xhr.open( endpoint, 'POST' );
+
+            xhr.send( payload );
+        } );
+    }
+}
+
+export default ajax;
+
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Ajax
+
+A simple little wrapper around XMLHttpRequest to promisify getting and sending
+data.
+
+## Install
+
+Eventually I'll work out how to publish my stuff to npm to make it as simple as
+`npm install --save @deanacus/ajax` but for the time being, it should be as simple
+as `curl -o /output/path/filename.js https://raw.githubusercontent.com/deanacus/ajax/ajax.js`
+
+## Usage
+
+Import the library into your file:
+
+`import 'ajax'`
+
+Then, depending on if you want to get data, or send data, use the appropriate interface.
+
+### Get
+
+To request data, call `ajax.get()`
+
+Paramaters:
+
+| Option   | Value type | Description                                                        |
+|----------|------------|--------------------------------------------------------------------|
+| endpoint | string     | The URL to which you would like to make the request                |
+| headers  | object     | An object containing the key/value pairs to set as request headers |
+
+
+### Send
+
+To send data, call `ajax.send()`
+
+Paramaters:
+
+| Option   | Value type | Description                                                                |
+|----------|------------|----------------------------------------------------------------------------|
+| endpoint | string     | The URL to which you would like to send the request                        |
+| payload  | any        | The data you wish to send to the enpoint. This is not processed in any way |
+| headers  | object     | An object container the key/value pairs to set as request headers          |
